7ffac3cbf3bfe066cbf77f05000787fc858af2e3
SHIVAMSINGH0101 <48352226+SHIVAMSINGH0101@users.noreply.github.com>
2020-01-30 00:36:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,179 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+//Requests targeting all articles
+//using express chainable route function app.route()
+app.route("/articles")
+.get(function(req, res){
+
+  Article.find(function(err, foundArticles){
+    if(!err)
+      res.send(foundArticles);
+    else
+        res.send(err);
+  });
+
+})
+.post(function(req, res){
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err)
+      res.send("Successfully added a new article");
+    else
+        res.send(err);
+  });
+
+})
+.delete(function(req, res){
+
+  Article.deleteMany(function(err){
+    if(!err)
+      //console.log("Deleted Successfully");
+      res.send("Deleted Successfully");
+    else
+        //console.log("Unsuccessful");
+        res.send(err);
+  });
+
+});
+
+
+
+
+// //making a get request from the browser and serving the data to browser from database
+// app.get("/articles", function(req, res){
+//
+//   Article.find(function(err, foundArticles){
+//     if(!err)
+//       res.send(foundArticles);
+//     else
+//         res.send(err);
+//   });
+//
+// });
+//
+//
+// //getting data from post request by postman and saving data in database. postman will send data through the mentioned route
+// //i.e. API ("/articles")
+// app.post("/articles", function(req, res){
+//   console.log(req.body.title);
+//   console.log(req.body.content);
+//
+//   const newArticle = new Article({
+//     title: req.body.title,
+//     content: req.body.content
+//   });
+//
+//   newArticle.save(function(err){
+//     if(!err)
+//       res.send("Successfully added a new article");
+//     else
+//         res.send(err);
+//   });
+//
+// });
+//
+//
+// app.delete("/articles", function(req, res){
+//
+//   Article.deleteMany(function(err){
+//     if(!err)
+//       //console.log("Deleted Successfully");
+//       res.send("Deleted Successfully");
+//     else
+//         //console.log("Unsuccessful");
+//         res.send(err);
+//   });
+//
+// });
+
+
+
+// request targeting specific article
+
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if(!err)
+      res.send(foundArticle);
+    else
+        res.send("No article found");
+  });
+
+})
+
+.put(function(req, res){
+
+  Article.update({title: req.params.articleTitle},
+                 {title: req.body.title, content:req.body.content},
+                 {overwrite: true},
+               function(err, results){
+                 if(!err)
+                  res.send("Successfully updated");
+               });
+
+})
+
+.patch(function(req, res){
+
+  Article.update({title: req.params.articleTitle},
+                {$set: req.body},
+                function(err){
+                  if(!err)
+                   res.send("Successfully updated");
+                  else
+                    res.send(err);
+                });
+
+})
+
+.delete(function(req, res){
+
+  Article.update({title: req.params.articleTitle},
+                function(err){
+                  if(!err)
+                    //console.log("Deleted Successfully");
+                    res.send("Deleted Successfully");
+                  else
+                      //console.log("Unsuccessful");
+                      res.send(err);
+                });
+
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
